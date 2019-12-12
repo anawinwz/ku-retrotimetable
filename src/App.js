edcdda034c859c264c6bb213743929ca2beaf01b
Anawin W <imonmac@hotmail.com>
2019-12-12 12:21:11 +0700

    Add Analytics events

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import KUTable from './components/KUTable';
 
+const iseaParser = require('./utils/iseaParser');
 const defaultCourses = {
   1: [],
   2: [],
@@ -20,8 +21,33 @@ function App() {
 
   const handleChange = e => {
     setIseaData(e.target.value);
-    setCourses(require('./utils/iseaParser').default(e.target.value));
+
+    const parsedValue = iseaParser.default(e.target.value);
+    const isSuccess = Object.keys(parsedValue).find(day => parsedValue[day].length > 0);
+
+    ReactGA.event({
+      category: 'Parse',
+      action: isSuccess ? 'Success' : 'Failed'
+    });
+    setCourses(parsedValue);
   };
+
+  const handleClear = () => {  
+    ReactGA.event({
+      category: 'Table',
+      action: 'Cleared'
+    });
+    setIseaData('');
+    setCourses(defaultCourses);
+  };
+
+  const handlePrint = () => {
+    ReactGA.event({
+      category: 'Table',
+      action: 'Printed via button'
+    });
+    window.print();
+  }
 
   useEffect(() => {
     ReactGA.initialize('UA-154189889-2');
@@ -34,7 +60,7 @@ function App() {
         <img src={require('./assets/example.jpg')} alt="ภาพตัวอย่างการคัดลอกข้อมูลจาก KU-ISEA" style={{width: '50%', minWidth: '320px'}} />
         <h3>Copy ข้อมูล<i>ที่คลุมดำตามภาพ</i> จาก <a href="https://isea.ku.ac.th/" rel="noopener noreferrer" target="_blank">KU-ISEA</a> เมนู 958<br />มา Paste ลงในช่องว่าง เพื่อสร้างตารางเรียนแบบดั้งเดิม</h3>
         <p><textarea onChange={handleChange} placeholder="วางข้อมูลที่ได้มาจากการ Copy ตามภาพ" value={iseaData} onMouseOver={e => e.target.select()} /></p>
-        <button onClick={() => {setIseaData(''); setCourses(defaultCourses);}} disabled={!iseaData}>เคลียร์ข้อมูล</button> <button onClick={() => window.print()}>พิมพ์ตารางเรียน</button>
+        <button onClick={handleClear} disabled={!iseaData}>เคลียร์ข้อมูล</button> <button onClick={handlePrint}>พิมพ์ตารางเรียน</button>
       </center>
       <div id="KUTable-container" style={{}/* {width: '100%', overflowY: 'auto'} */}>
         <KUTable courses={courses} />
